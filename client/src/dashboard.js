a5b15c148836b7b99d64cdae37183e38b11f1666
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2018-01-14 17:08:15 -0600

    added dashboard shell from bootstrap lite

---
+++ client/src/dashboard.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// This page holds the Dashboard Shell Component
+
+class Dashboard extends Component {
+	render () {
+		return (
+			<div className="Dashboard">
+	        	<div className="wrapper">
+        			<div className="sidebar" data-color="blue" data-image="assets/images/network.png">
+				    	<div className="sidebar-wrapper">
+				            <div className="logo">
+				                <a href="#" className="simple-text">
+				                    PrepConnect
+				                </a>
+				            </div>
+				            <ul className="nav">
+				                <li className="active">
+				                    <a href="#">
+				                        <i className="pe-7s-user"></i>
+				                        <p>Profile</p>
+				                    </a>
+				                </li>
+				                <li>
+				                    <a href="#">
+				                        <i className="pe-7s-plus"></i>
+				                        <p>Post a Job</p>
+				                    </a>
+				                </li>
+				                <li>
+				                    <a href="#">
+				                        <i className="pe-7s-share"></i>
+				                        <p>Connections</p>
+				                    </a>
+				                </li>
+				                <li>
+				                    <a href="#">
+				                        <i className="pe-7s-display2"></i>
+				                        <p>Prep</p>
+				                    </a>
+				                </li>
+				                <li>
+				                    <a href="#">
+				                        <i className="pe-7s-copy-file"></i>
+				                        <p>Current Postings</p>
+				                    </a>
+				                </li>
+				            </ul>
+				    	</div>
+    				</div>
+
+				    <div className="main-panel">
+				        <nav className="navbar navbar-default navbar-fixed">
+				            <div className="container-fluid">
+				                <div className="navbar-header">
+				                    <button type="button" className="navbar-toggle" data-toggle="collapse">
+				                        <span className="sr-only">Toggle navigation</span>
+				                        <span className="icon-bar"></span>
+				                        <span className="icon-bar"></span>
+				                        <span className="icon-bar"></span>
+				                    </button>
+				                    <a className="navbar-brand" href="#">User Name</a>
+				                </div>
+				                <div className="collapse navbar-collapse">
+				                    <ul className="nav navbar-nav navbar-left">
+				                        <li>
+				                            <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+				                                <i className="fa fa-dashboard"></i>
+				                            </a>
+				                        </li>
+				                    </ul>
+
+				                    <ul className="nav navbar-nav navbar-right">
+				                        <li>
+				                           <a href="#">
+				                               Logout
+				                            </a>
+				                        </li>
+				                    </ul>
+				                </div>
+				            </div>
+				        </nav>
+
+				        <div className="content">
+				            <div className="container-fluid">
+				                <div className="row">
+
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+			</div>
+
+		)
+	}
+}
+
+export default Dashboard;
