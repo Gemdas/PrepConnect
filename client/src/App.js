9220d43d7858454b9d9b4ced5390cdaa81f829f4
Sam Sadler <samsadler@sams-mbp.attlocal.net>
2018-01-14 08:33:31 -0600

    adding rough login and createAccount

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// This file holds the landing/login componenent
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="wrapper">
+            <div className="card-body row justify-content-center">
+              <div className="col-sm-6 loginForm">
+              <div className="card-header">
+                <h1 className="card-title">Welcome to PrepConnect</h1>
+                <h3 className="card-subtitle mb-2 text-muted">Nail the Interview!</h3>
+                <h4 className="card-text">Interviewing made easy. Create an account or log in to review actual interview questions, or initiate an interview for a job matching your skill!</h4>              
+              </div>
+              <br />
+              <form>
+                <div className="form-group">
+                  <label className="" htmlFor="exampleInputEmail1">Email address</label>
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                  <div className="row">
+                      <div className="col-sm-6">
+                        <button type="submit" className="btn btn-outline">Submit</button>
+                      </div>
+                      <div className="col-sm-6">
+                        <button type="submit" className="btn btn-outline">Create an account</button>
+                      </div>
+                  </div>
+              </form>
+              </div>
+            </div>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CreateAccount from './createAccount';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<CreateAccount />, document.getElementById('root'));
+
