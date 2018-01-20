0e9b0bc421dbc8871331df7026b2bdbea4d2268c
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2018-01-20 11:19:41 -0600

    Adds checkbox to login page, handles state

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -6,9 +6,41 @@ import './login.css';
 // Still have to work on the Create Account
 
 class Login extends Component {
+
+  
+    state = { 
+      checkboxChecked: false, 
+      email: "", 
+      password: ""
+    };
+    
+    // this.handleChange = this.handleChange.bind(this);
+
+    handleChange = event => {
+      const { value } = event.target;
+
+      if (this.state.checkboxChecked) {
+        this.setState({
+          checkboxChecked: false
+        })
+      } else {
+        this.setState({
+          checkboxChecked: true
+        })
+      }
+    }
+    
+    handleSubmit = event => {
+      event.preventDefault();
+    }
+    
+    handleCreateAccount = event => {
+      event.preventDefault();
+    }
+
   render() {
     return (
-      <div className="wrapper" data-color="blue" data-image="http://images.all-free-download.com/images/graphiclarge/seamless_network_background_312309.jpg">
+      <div className="wrapper loginBackground">
           <div className="card-body row align-items-center">
           
             <div className="col-sm-6 loginForm">
@@ -20,21 +52,27 @@ class Login extends Component {
             <br />
             <form>
               <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <label htmlFor="email">Email address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.email}/>
               </div>
               <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" placeholder="Password" value={this.state.password}/>
+              </div>
+              <div className="form-check recruiterCheck">
+                <input className="form-check-input" type="checkbox" value="" id="role" checked={this.state.checkboxChecked} onChange={this.handleChange}/>
+                <label className="form-check-label" htmlFor="role">
+                  Recruiter
+                </label>
               </div>
                 <div className="row">
                     <div className="col-sm-6">
                     <Link to="/dashboard">
-                      <button type="submit" className="btn btn-outline">Submit</button>
+                      <button type="submit" className="btn btn-outline" onSubmit={this.handleSubmit}>Submit</button>
                     </Link>
                     </div>
                     <div className="col-sm-6">
-                      <button type="submit" className="btn btn-outline">Create an account</button>
+                      <button type="submit" className="btn btn-outline" onSubmit={this.handleCreateAccount}>Create an account</button>
                     </div>
                 </div>
             </form>
