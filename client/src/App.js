3dcaa42c5c9613a3cbf1e4c8129cc2ae8c9e61fb
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2018-01-21 14:30:16 -0600

    Adds okta demo files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+/* import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./Pages/Login";
+import Login from "./Components/Authentication/LoginPage";
 import Dashboard from "./Pages/Dashboard";
 
 
@@ -14,4 +14,33 @@ const App = () =>
     </div>
   </Router>;
 
-export default App;
+export default App; */
+
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+import Navigation from './Components/Shared/Navigation';
+import HomePage from './Components/Home/HomePage';
+import RegistrationForm from './Components/Authentication/RegistrationForm';
+import config from './app.config';
+import LoginPage from './Components/Authentication/LoginPage';
+import ProfilePage from './Components/Authentication/ProfilePage';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <main>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" render={() => <LoginPage baseUrl={config.url} />} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <Route path="/register" component={RegistrationForm} />
+          <SecureRoute path="/profile" component={ProfilePage} />
+        </main>
+      </div>
+    );
+  }
+}
