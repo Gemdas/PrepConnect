31b70c2caf7eae93df1fa2379bf1b07df1f193ea
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2018-01-27 09:07:08 -0600

    Reroutes server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./server/routes");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 var cookieParser = require('cookie-parser');
